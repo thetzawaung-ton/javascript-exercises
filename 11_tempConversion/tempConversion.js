30a022b46ba83f066a1a22ce80a8d66cdd99a010
thetzawaung-ton <241034270+thetzawaung-ton@users.noreply.github.com>
2025-12-18 09:46:50 +0630

    Add solution for 11
    My solution is a bit longer and messier

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsiusValue = ((fahrenheit - 32) * 5 / 9);
+  if (!Number.isInteger(celsiusValue)) {
+    celsiusValue = Number(celsiusValue.toFixed(1));
+  }
+  return celsiusValue;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheitValue = (celsius * 9 / 5 + 32);
+  if(!Number.isInteger(fahrenheitValue)) {
+    fahrenheitValue = Number(fahrenheitValue.toFixed(1));
+  } 
+  return fahrenheitValue;
 };
 
 // Do not edit below this line
