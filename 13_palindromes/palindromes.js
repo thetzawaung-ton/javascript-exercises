5e31adc33798aec69b4cdd02f4f175d676f58946
thetzawaung-ton <241034270+thetzawaung-ton@users.noreply.github.com>
2025-12-29 11:55:55 +0630

    Submit the answer _palindromes
    I'd done some googling and thought I have to use regex

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let trimStr = str.replace(/[^a-zA-Z0-9]/g, '').toUpperCase();
+    let reverseStr = trimStr.split('').reverse().join('');
 
+    return trimStr === reverseStr;
 };
 
 // Do not edit below this line
