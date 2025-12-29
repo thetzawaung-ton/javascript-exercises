1673319c670706642a912e939c22f42f96abfaa7
thetzawaung-ton <241034270+thetzawaung-ton@users.noreply.github.com>
2025-12-29 10:20:25 +0630

    Submit the answer for 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((multiply, num) => multiply * num);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  for(let i = num; i >= 1; i--){
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
