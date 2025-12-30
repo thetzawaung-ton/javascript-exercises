24894a442aa61a2c96c81d443d1c73c26bc53ce9
thetzawaung-ton <241034270+thetzawaung-ton@users.noreply.github.com>
2025-12-30 10:00:17 +0630

    Submit the answer for 15_findTheOldest
    My solution is a little different, I used sort assign in a variable.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const todayDate = new Date();
+    const sortByAge = array.sort((a,b) => 
+        ((b.yearOfDeath || todayDate.getFullYear()) - b.yearOfBirth) - 
+        ((a.yearOfDeath || todayDate.getFullYear()) - a.yearOfBirth));
+    return sortByAge[0];
 };
 
 // Do not edit below this line
