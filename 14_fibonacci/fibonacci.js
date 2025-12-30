5a204c23c979db1911e128eaa1820882e5302c13
thetzawaung-ton <241034270+thetzawaung-ton@users.noreply.github.com>
2025-12-30 09:34:49 +0630

    Submit the answer for 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num == 0) {
+        return 0;
+    } else if (num < 0) {
+        return "OOPS";
+    }
+    let fibo = [1, 1];
+    for(let i = 2; i <= num; i++){
+        let next = fibo[i - 1] + fibo[i - 2];
+        fibo.push(next);
+    } 
+    return fibo[num - 1];
 };
 
 // Do not edit below this line
