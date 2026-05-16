4f541ff837bf1b79f56f02f9645c42bf5d8a5ed4
thetzawaung-ton <241034270+thetzawaung-ton@users.noreply.github.com>
2026-05-16 09:58:14 +0630

    Add solution for pascal
    My solution is not as good as example solution.

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,5 +1,13 @@
-const pascal = function() {
-  
+const pascal = function(input, result = []) {
+    if(input === 1) {
+        result.push(input);
+    } else {
+        const add0ToResult = [0].concat(pascal(input - 1), [0]);
+        for(let i = 0; i < add0ToResult.length - 1; i++) {
+            result.push(add0ToResult[i] + add0ToResult[i + 1]);
+        }
+    }
+    return result;
 };
   
 // Do not edit below this line
